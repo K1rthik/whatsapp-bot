43c30e37c84d74448e49490a8d799266f40a531b
Karthik <k1rthik1172@gmail.com>
2026-01-16 16:51:12 +0530

    whatsapp

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.post("/webhook", async (req, res) => {
 /**
  * START SERVER (Railway safe)
  */
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Bot running on port ${PORT}`);
 });
